fcc5d27490fd0bf56e35bce5dc8e75a6bb1258d8
renjunjia3 <renjunjia1>
2018-04-10 09:33:11 +0800

    修改统计

--- app/src/main/java/com/quduo/welfareshop/base/BaseActivity.java
+++ app/src/main/java/com/quduo/welfareshop/base/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.quduo.welfareshop.base;
 
 import com.hss01248.dialog.ActivityStackManager;
+import com.umeng.analytics.MobclickAgent;
 
 import me.yokeyword.fragmentation.SupportActivity;
 
@@ -20,5 +21,17 @@ public class BaseActivity extends SupportActivity {
     public void onDetachedFromWindow() {
         super.onDetachedFromWindow();
         ActivityStackManager.getInstance().removeTopAttached(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
     }
 }
